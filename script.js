bb4f0844ad8d3a0fa349f76fa2f69a1a8e03689f
EmikoParas-Personal_Portfolio <144320280+emikoparas@users.noreply.github.com>
2024-05-28 22:51:44 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+//Choose heading that has the ID
+var heading = document.getElementById('heeading');
+//Click event listener for heading
+heading.addEventListener('click', function() {
+//Adjust heading's color
+    this.style.color = 'navy';
+});
